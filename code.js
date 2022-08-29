71efe3c539133f362af202d6e1bbe695afaddc15
jimestir <jimmyestir@gmail.com>
2022-08-29 23:02:40 +0200

    Add operations

---
+++ code.js
@@ -0,0 +1,8 @@
+const operations = {
+  addition: (a, b) => a + b,
+  subtraction: (a, b) => a - b,
+  division: (a, b) => a / b,
+  multiplication: (a, b) => a * b,
+  empowerment: (a, b) => Math.pow(a, b),
+  reversSign: (a) => a * -1,
+};
