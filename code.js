d0bd2af1e47ad39dc72233906648906fbeca50ad
jimestir <jimmyestir@gmail.com>
2022-08-29 23:05:40 +0200

    Add functions of write, of erase and erase everything

--- code.js
+++ code.js
@@ -6,3 +6,31 @@ const operations = {
   empowerment: (a, b) => Math.pow(a, b),
   reversSign: (a) => a * -1,
 };
+
+const number = document.querySelectorAll("number");
+const btn = document.querySelector(".calculator__buttons");
+const displayValue = document.querySelector(".display-value");
+const r = /[0-9]/;
+
+btn.addEventListener("click", (e) => {
+  const keyword = e.target.value;
+  if (r.test(keyword)) {
+    writeToDOM(keyword);
+  }
+  if (keyword === "C") {
+    erase(displayValue);
+  }
+  if (keyword === "AC") {
+    eraseEverything(displayValue);
+  }
+});
+
+function writeToDOM(content) {
+  displayValue.innerHTML += content;
+}
+function erase(element) {
+  element.innerHTML = element.innerHTML.slice(0, -1);
+}
+function eraseEverything(element) {
+  element.innerHTML = "";
+}
